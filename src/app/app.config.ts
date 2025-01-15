e2630b2e2485365525b30924c3ab049d6b81416e
Andrea Mandini <a.mandini@mauti.it>
2025-01-15 22:54:20 +0100

    feat: http interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,13 @@
 import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
-import {provideHttpClient} from '@angular/common/http';
+import {provideHttpClient, withInterceptors} from '@angular/common/http';
+import {httpErrorInterceptor} from './shared/http-error.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideAnimationsAsync(),
-    provideHttpClient(),]
+    provideHttpClient(
+      withInterceptors([httpErrorInterceptor])
+    ),]
 };
